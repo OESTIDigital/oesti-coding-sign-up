1d9f638fa54feaeaadc8f58f307f29e0360d2c3a
Felippe Rodrigo Puhle <lippezzin@gmail.com>
2016-09-07 20:35:43 -0300

    Add unique index to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 	has_secure_password
-	validates :name, :email, presence: true
+
+	validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
 	scope :all_except, ->(user) { where.not(id: user) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907225729) do
+ActiveRecord::Schema.define(version: 20160907233307) do
 
   create_table "exercises", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160907225729) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
